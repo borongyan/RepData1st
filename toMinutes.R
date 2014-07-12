e401a5f78b2be255d494fe05bf86e67c72e6f47d
daviesbj <daviesbj@bp.com>
2014-07-12 17:37:17 +0100

    Added toMinutes

--- toMinutes.R
+++ toMinutes.R
@@ -1,6 +1,6 @@
-toMinutes <- function( interval ){
-  interval <- as.integer(interval)
-  hours_part <- floor(interval/100)
+Interval2Minutes <- function( interval ){
+  interval <- as.integer( interval )
+  hours_part <- floor( interval / 100 )
   minutes_part <- interval - 100 * hours_part
   minutes <- hours_part * 60 + minutes_part
 minutes
