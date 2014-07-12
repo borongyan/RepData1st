7b44788c12980afaceec87f7e68c4cd9fd60df5f
Brian Davies <daviesbj@users.noreply.github.com>
2014-07-12 20:23:28 +0100

    Duplicate function -- misnamed

--- toMinutes.R
+++
@@ -1,7 +0,0 @@
-Interval2Minutes <- function( interval ){
-  interval <- as.integer( interval )
-  hours_part <- floor( interval / 100 )
-  minutes_part <- interval - 100 * hours_part
-  minutes <- hours_part * 60 + minutes_part
-minutes
-}
